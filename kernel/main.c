bbc2dc720c3a507bf879a731ec79f1ea42f5263b
Tang Qiming <missingjs@outlook.com>
2023-03-04 09:13:43 +0800

    function put_int

--- kernel/main.c
+++ kernel/main.c
@@ -1,32 +1,15 @@
 #include "kernel/print.h"
 void main(void) {
-    put_char('k');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
-    put_char('\n');
-    put_char('m');
-    put_char('i');
-    put_char('s');
-    put_char('s');
-    put_char('i');
-    put_char('n');
-    put_char('g');
-    put_char('j');
-    put_char('s');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\b');
-    put_char('3');
-    int i=100;
-    while(i--)
-    {
-        put_char('x');
-    }
-    put_char('\n');
     put_str("I'm a kernel\n");
+    put_str("missingjs 2023-03-04 09:12 hi~\n");
+    put_int(0);
+    put_char('\n');
+    put_int(9);
+    put_char('\n');
+    put_int(0x00021a3f);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x00000000);
     while(1);
 }
