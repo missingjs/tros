fa476e3c660b76f2386f216900393ae42ebc2efc
Tang Qiming <missingjs@outlook.com>
2023-03-07 21:37:33 +0800

    restructure files

---
+++ src/kernel/main.c
@@ -0,0 +1,15 @@
+#include "lib/kernel/print.h"
+void main(void) {
+    put_str("I'm a kernel\n");
+    put_str("missingjs 2023-03-04 09:12 hi~\n");
+    put_int(0);
+    put_char('\n');
+    put_int(9);
+    put_char('\n');
+    put_int(0x00021a3f);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x00000000);
+    while(1);
+}
