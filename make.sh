b465bb4dcbd995b552511251d4013d0a820e3a65
Tang Qiming <missingjs@outlook.com>
2023-03-07 21:38:36 +0800

    optimize build process

--- make.sh
+++ make.sh
@@ -6,17 +6,20 @@ mkdir build build/boot build/kernel build/lib build/lib/kernel
 
 set -xe
 
-nasm -I ./boot/include -o ./build/boot/mbr.bin ./boot/mbr.S
+# nasm -I ./boot/include -o ./build/boot/mbr.bin ./boot/mbr.S
+nasm -I ./include -o ./build/boot/mbr.bin ./src/boot/mbr.S
 
 dd if=./build/boot/mbr.bin of=./hd60M.img bs=512 count=1 conv=notrunc
 
-nasm -I ./boot/include -o ./build/boot/loader.bin ./boot/loader.S
+# nasm -I ./boot/include -o ./build/boot/loader.bin ./boot/loader.S
+nasm -I ./include -o ./build/boot/loader.bin ./src/boot/loader.S
 
 dd if=./build/boot/loader.bin of=./hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
-nasm -f elf32 -o ./build/lib/kernel/print.o ./lib/kernel/print.S
+nasm -f elf32 -o ./build/lib/kernel/print.o ./src/lib/kernel/print.S
 
-gcc -m32 -fno-pic -I./lib -c -o ./build/kernel/main.o kernel/main.c
+# gcc -m32 -fno-pic -I./lib -c -o ./build/kernel/main.o ./src/kernel/main.c
+gcc -m32 -fno-pic -I ./include -c -o ./build/kernel/main.o ./src/kernel/main.c
 # gcc -m32 -c -O1 -o ./build/kernel/main.o kernel/main.c
 
 objcopy --remove-section .note.gnu.property ./build/kernel/main.o
