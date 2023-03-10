180aca0773126ee2106b68ddc28add752a49a017
Qiming Tang <qiming.tang@tusen.ai>
2023-03-10 15:21:51 +0800

    simple thread

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,18 +1,28 @@
-#include "lib/kernel/print.h"
 #include "kernel/init.h"
-#include "kernel/memory.h"
-int main(void) {
-   put_str("I am kernel\n");
-   init_all();
+#include "lib/kernel/print.h"
+#include "thread/thread.h"
 
-   void* addr = get_kernel_pages(3);
-   put_str("\n get_kernel_page start vaddr is ");
-   put_int((uint32_t)addr);
-   put_str("\n");
-   void* addr2 = get_kernel_pages(2);
-   put_str("\n get_kernel_page start vaddr2 is ");
-   put_int((uint32_t)addr2);
-   put_str("\n");
-   while(1);
-   return 0;
+void k_thread_a(void*);
+
+int main(void) {
+    put_str("I am kernel\n");
+    init_all();
+
+    thread_start("k_thread_a", 31, k_thread_a, "K-thread-a-argA ");
+
+    while(1);
+    return 0;
+}
+
+/* 在线程中运行的函数 */
+void k_thread_a(void* arg) {
+    /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+    char* para = arg;
+    int32_t number = 0;
+    while(1) {
+        put_str(para);
+        put_char(' ');
+        put_int(number++);
+        put_char('\n');
+    }
 }
