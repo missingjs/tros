1dc4b5df257891ea3cfd55e0cdc2fe9e25504f34
Tang Qiming <missingjs@outlook.com>
2023-03-11 10:25:55 +0800

    semaphore, lock and console

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,3 +1,4 @@
+#include "device/console.h"
 #include "device/timer.h"
 #include "kernel/init.h"
 #include "kernel/interrupt.h"
@@ -9,7 +10,8 @@
 void init_all() {
    put_str("init_all\n");
    idt_init();    // 初始化中断
-   timer_init();  // 初始化PIT
    mem_init();	  // 初始化内存管理系统
    thread_init(); // 初始化线程相关结构
+   timer_init();  // 初始化PIT
+   console_init(); //控制台初始化最好放在开中断之前
 }
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,20 +1,26 @@
+#include "device/console.h"
 #include "kernel/init.h"
-#include "lib/kernel/print.h"
-#include "thread/thread.h"
 #include "kernel/interrupt.h"
+#include "thread/thread.h"
+#include "lib/kernel/print.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
+void k_thread_c(void*);
+void k_thread_d(void*);
+
 int main(void) {
    put_str("I am kernel\n");
    init_all();
 
-   thread_start("k_thread_a", 31, k_thread_a, "argA ");
-   thread_start("k_thread_b", 8, k_thread_b, "argB ");
+   thread_start("k_thread_a", 64, k_thread_a, "argA ");
+   thread_start("k_thread_b", 32, k_thread_b, "argB ");
+   thread_start("k_thread_c", 16, k_thread_c, "argC ");
+   thread_start("k_thread_d", 8, k_thread_d, "argD ");
 
-   intr_enable();	// 打开中断,使时钟中断起作用
+   intr_enable();
    while(1) {
-      put_str("Main ");
+      console_put_str("Main ");
    };
    return 0;
 }
@@ -24,7 +30,7 @@ void k_thread_a(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
    while(1) {
-      put_str(para);
+      console_put_str(para);
    }
 }
 
@@ -33,6 +39,24 @@ void k_thread_b(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
    while(1) {
-      put_str(para);
+      console_put_str(para);
+   }
+}
+
+/* 在线程中运行的函数 */
+void k_thread_c(void* arg) {
+/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+   char* para = arg;
+   while(1) {
+      console_put_str(para);
+   }
+}
+
+/* 在线程中运行的函数 */
+void k_thread_d(void* arg) {
+/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+   char* para = arg;
+   while(1) {
+      console_put_str(para);
    }
 }
