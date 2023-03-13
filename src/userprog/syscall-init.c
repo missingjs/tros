d598ddb957a0eb989c56fc2a04256ad20acd4fc2
Qiming Tang <qiming.tang@tusen.ai>
2023-03-13 15:30:37 +0800

    user space function printf

--- src/userprog/syscall-init.c
+++ src/userprog/syscall-init.c
@@ -1,5 +1,7 @@
+#include "device/console.h"
 #include "lib/kernel/print.h"
 #include "lib/stdint.h"
+#include "lib/string.h"
 #include "lib/user/syscall.h"
 #include "thread/thread.h"
 #include "userprog/syscall-init.h"
@@ -13,9 +15,16 @@ uint32_t sys_getpid(void) {
    return running_thread()->pid;
 }
 
+/* 打印字符串str(未实现文件系统前的版本) */
+uint32_t sys_write(char* str) {
+   console_put_str(str);
+   return strlen(str);
+}
+
 /* 初始化系统调用 */
 void syscall_init(void) {
    put_str("syscall_init start\n");
    syscall_table[SYS_GETPID] = sys_getpid;
+   syscall_table[SYS_WRITE] = sys_write;
    put_str("syscall_init done\n");
 }
