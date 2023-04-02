f559af2c818d72e549311f89ed98b9afb7383b8d
Tang Qiming <missingjs@outlook.com>
2023-04-02 14:45:13 +0800

    re-format code

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -38,25 +38,30 @@ int main(void) {
 /*************    写入应用程序结束   *************/
    cls_screen();
 //   console_put_str("[rabbit@localhost /]$ ");
-//   thread_exit(running_thread(), true);
-   while (1) {
-       thread_yield();
-   }
+  thread_exit(running_thread(), true);
+   // while (1) {
+   //     thread_yield();
+   // }
    return 0;
 }
 
 /* init进程 */
-void init(void) {
+void init(void)
+{
    uint32_t ret_pid = fork();
-   if(ret_pid) {  // 父进程
+   if (ret_pid)
+   { // 父进程
       int status;
       int child_pid;
-       /* init在此处不停的回收僵尸进程 */
-       while(1) {
-	  child_pid = wait(&status);
-	  printf("I`m init, My pid is 1, I recieve a child, It`s pid is %d, status is %d\n", child_pid, status);
-       }
-   } else {	  // 子进程
+      /* init在此处不停的回收僵尸进程 */
+      while (1)
+      {
+         child_pid = wait(&status);
+         printf("I`m init, My pid is 1, I recieve a child, It`s pid is %d, status is %d\n", child_pid, status);
+      }
+   }
+   else
+   { // 子进程
       my_shell();
    }
    panic("init: should not be here");
