2980845fa0441336c6d7990f4079fa816b4191d9
Tang Qiming <missingjs@outlook.com>
2023-04-18 23:44:50 +0800

    define file i/o interface

--- system/crt/stdio.h
+++ system/crt/stdio.h
@@ -5,5 +5,17 @@ typedef char* va_list;
 uint32_t printf(const char* str, ...);
 uint32_t vsprintf(char* str, const char* format, va_list ap);
 uint32_t sprintf(char* buf, const char* format, ...);
+
+struct file_object;
+
+typedef struct {
+    struct file_object *handle;
+} FILE;
+
+FILE *fopen(const char *filename, const char *mode);
+void fclose(FILE *fp);
+uint32_t fscanf(FILE *fp, const char *format, ...);
+uint32_t fprintf(FILE *fp, const char *format, ...);
+
 #endif
 
